831e028a68d2c18f0c34b56099541d4ea6b072aa
ypat0207 <ypat0207@gmail.com>
2023-02-17 11:34:31 -0600

    Parameterize Test import removed
    removed unused library(parameterize)

--- HomeworkTwo/src/test/java/SavingsMethodTest.java
+++ HomeworkTwo/src/test/java/SavingsMethodTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SavingsMethodTest {
